2b925629248624286d4a067639fe6702c831ae3f
ABHISHEK-XPS15\Abhishek Biju <abhishek.das@datafortune.com>
2023-12-10 07:12:43 -0600

    Background listener functionality

--- contentScript.js
+++ contentScript.js
@@ -1,3 +1,12 @@
 (() => {
-    let timer;
+    let youtubeLeftControls, youtubePlayer;
+    let currentVideo = "";
+
+    chrome.runtime.onMessgge.addListener((obj, sender, responce) => {
+        const { type, value, videoId } = obj;
+        if (type === "NEW") {
+            currentVideo = videoId;
+            newVideoLoaded();
+        }
+    })
 })();
